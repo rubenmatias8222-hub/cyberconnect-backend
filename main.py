6cf5a13ee152a8f198db058e6ddb0af05d8131b0
rubenmatias8222-hub <rubenmatias8222@gmail.com>
2026-05-16 13:13:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
+# Allow frontend access
+origins = [
+    "https://rubenmatias8222-hub.github.io"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Validation model
+class ContactForm(BaseModel):
+    name: str
+    email: EmailStr
+    message: str
 
 @app.get("/")
 def home():
     return {"message": "CyberConnect backend running"}
 
 @app.post("/contact")
-def contact(data: dict):
+def contact(form: ContactForm):
+
+    print("New message received:")
+    print(form)
+
     return {
-        "success": True,
-        "message": "Message received",
-        "data": data
+        "message": "Message sent successfully!"
     }
